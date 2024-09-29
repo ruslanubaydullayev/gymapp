cb97fcd2b21bedae8addf18df88266e51936096f
activus <ubaydullayev.r2712@gmail.com>
2024-09-29 17:00:10 +0500

    refactor: add athlete

--- src/pages/athletes/add.vue
+++ src/pages/athletes/add.vue
@@ -5,89 +5,67 @@
         <div class="flex items-center justify-between">
           <el-button type="primary" @click="routeBack"> Orqaga </el-button>
         </div>
-        <h5 class="text-2xl mt-4 font-semibold text-center">Sotuv qo'shish</h5>
-        <el-select
-          v-model="form.type"
-          placeholder="Turni tanlang"
-          size="medium"
-          style="width: 240px"
-          class="!w-full mt-2"
-        >
-          <el-option
-            v-for="item in types"
-            :key="item.value"
-            :value="item.name"
-          />
-        </el-select>
+        <h5 class="text-2xl mt-4 font-semibold text-center">Atlet qo'shish</h5>
         <el-input
-          v-model="form.product"
-          placeholder="Mahsulot nomini kiriting"
+          v-model="form.fullName"
+          placeholder="To'liq ismni kiriting"
           class="mt-2"
         />
-        <el-input
-          v-model="form.price"
-          type="number"
-          placeholder="Mahsulot narxini kiriting"
-          class="mt-2"
-        />
-        <el-input
-          v-model="form.amount"
-          type="number"
-          placeholder="Mahsulot miqdorini kiriting"
-          class="mt-2"
-        />
-        <div class="flex items-center flex-wrap gap-2 mt-3">
-          <el-select
-            v-model="form.seller"
-            placeholder="Sotuvchini tanlang"
-            size="medium"
-            style="width: 240px"
-            class="!w-full"
-          >
-            <el-option
-              v-for="item in sellers"
-              :key="item.value"
-              :value="item.name"
-            />
-          </el-select>
 
-          <el-select
-            v-model="form.buyer"
-            placeholder="Sotib oluvchini tanlang"
-            size="medium"
-            style="width: 240px"
-            class="!w-full"
-          >
-            <el-option
-              v-for="item in buyers"
-              :key="item.value"
-              :value="item.name"
-            />
-          </el-select>
-        </div>
+        <el-radio-group v-model="form.coachRequired">
+          <el-radio value="true" size="large">Trenerli</el-radio>
+          <el-radio value="false" size="large">Trenersiz</el-radio>
+        </el-radio-group>
 
-        <el-upload
-          v-model:file-list="fileList"
-          class="upload-demo"
-          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-          multiple
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :before-remove="beforeRemove"
-          :limit="3"
-        >
-          <el-button type="primary" class="!mx-auto mt-2"
-            >Rasm jo'natish</el-button
-          >
-          <template #tip>
-            <div class="el-upload__tip">
-              Hajmi 500Kb dan ortmaydigan rasm jo'nata olasiz.
-            </div>
-          </template>
-        </el-upload>
+        <el-input
+          v-model="form.phoneNumber"
+          type="number"
+          placeholder="Telefon raqamni kiriting"
+          class="mt-2"
+        />
+
+        <el-input
+          v-model="form.telegramUsername"
+          type="text"
+          placeholder="Telegram username"
+          class="mt-2"
+        />
+
+        <el-switch
+          v-model="form.prePayment"
+          class="my-2"
+          active-text="Avans berdi"
+          inactive-text="Avanssiz"
+        />
+
+        <el-input v-if="form.prePayment"
+          v-model="form.prePaymentAmount"
+          type="text"
+          placeholder="Miqdorni kiriting"
+          class="mt-2"
+        />
+
+        <el-switch
+          v-model="form.startLater"
+          class="my-2"
+          style="--el-switch-on-color: #13ce66; --el-switch-off-color: #409eff"
+          inactive-text="Bugundan boshlaydi"
+          active-text="Keyinroq boshlaydi"
+        />
+
+        <el-date-picker
+          v-if="form.startLater"
+          v-model="form.startDate"
+          type="date"
+          size="large"
+          placeholder="Kunni tanlang"
+        />
+
+
+
 
         <div class="flex items-center justify-center my-4">
-          <el-button type="primary">Jo'natish</el-button>
+          <el-button type="primary">Qo'shish</el-button>
         </div>
       </div>
     </div>
@@ -99,101 +77,18 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 const form = ref({
-  product: '',
-  type: '',
-  price: '',
-  amount: '',
-  seller: '',
-  buyer: '',
+  fullName: '',
+  coachRequired: '',
+  phoneNumber: '',
+  telegramUsername: '',
+  prePayment: false,
+  prePaymentAmount: '',
+  startLater: false,
+  startDate: ''
 })
 
 function routeBack() {
   router.back()
 }
 
-import type { UploadProps, UploadUserFile } from 'element-plus'
-import { ElMessageBox } from 'element-plus'
-
-const fileList = ref<UploadUserFile[]>([
-  {
-    name: 'element-plus-logo.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-  {
-    name: 'element-plus-logo2.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-])
-
-const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
-  console.log(file, uploadFiles)
-}
-
-const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
-  console.log(uploadFile)
-}
-
-const beforeRemove: UploadProps['beforeRemove'] = (uploadFile) => {
-  return ElMessageBox.confirm(
-    `Cancel the transfer of ${uploadFile.name} ?`,
-  ).then(
-    () => true,
-    () => false,
-  )
-}
-
-const types = [
-  {
-    name: 'Poliz',
-    value: 1,
-  },
-  {
-    name: 'Meva',
-    value: 2,
-  },
-  {
-    name: 'Sabzavot',
-    value: 3,
-  },
-  {
-    name: 'Ko\'kat',
-    value: 4,
-  },
-]
-const sellers = [
-  {
-    name: 'Abdulla',
-    value: 1,
-  },
-  {
-    name: 'Abbos',
-    value: 2,
-  },
-  {
-    name: 'Shahzod',
-    value: 3,
-  },
-  {
-    name: 'Marjona',
-    value: 4,
-  },
-]
-const buyers = [
-  {
-    name: 'Nozim',
-    value: 1,
-  },
-  {
-    name: 'Nazokat',
-    value: 2,
-  },
-  {
-    name: 'Fozil',
-    value: 3,
-  },
-  {
-    name: 'Asad',
-    value: 4,
-  },
-]
 </script>
